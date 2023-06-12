a7890e144b046d5df6274a1beb32f8c4272adb87
Anthony Limiero <normaljoepro@gmail.com>
2023-06-12 19:02:10 -0400

    Moves database creation to independent python file for ease of testing

--- database.py
+++ database.py
@@ -91,62 +91,6 @@ class Envelope:
     def __str__(self):
         return "ID:{}, NAME:{}, BAL:{}, BUDG:{}, DEL:{}, U_ID:{}, DISP:{}".format(self.id,self.name,self.balance,self.budget,self.deleted,self.user_id,self.display_order)
 
-
-def create_db():
-    """
-    Creates the database file
-    """
-    c.execute("""
-        CREATE TABLE transactions (
-            id INTEGER PRIMARY KEY,
-            type INTEGER NOT NULL,
-            name TEXT NOT NULL,
-            amount INTEGER NOT NULL,
-            day DATE NOT NULL,
-            envelope_id INTEGER,
-            account_id INTEGER,
-            grouping INTEGER NOT NULL,
-            note TEXT NOT NULL DEFAULT '',
-            schedule TEXT,
-            status BOOLEAN NOT NULL DEFAULT 0,
-            user_id NOT NULL,
-            a_reconcile_bal INTEGER NOT NULL DEFAULT 0,
-            e_reconcile_bal INTEGER NOT NULL DEFAULT 0
-            )
-        """)
-
-    c.execute("""
-        CREATE TABLE accounts (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            balance INTEGER NOT NULL DEFAULT 0,
-            deleted BOOLEAN NOT NULL DEFAULT 0,
-            user_id INTEGER NOT NULL
-            )
-        """)
-
-    c.execute("""
-        CREATE TABLE envelopes (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            balance INTEGER NOT NULL DEFAULT 0,
-            budget INTEGER NOT NULL DEFAULT 0,
-            deleted BOOLEAN NOT NULL DEFAULT 0,
-            display_order INTEGER,
-            user_id INTEGER NOT NULL
-            )
-        """)
-
-    c.execute("""
-        CREATE TABLE users (
-            user_id INTEGER PRIMARY KEY,
-            email TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL
-            )
-        """)
-
-    insert_envelope(Envelope('Unallocated', 0, 0, 0, USER_ID))
-
 # ----- Helper functions ----- #
 def unpack(list_of_tuples):
     unpacked_array = []
