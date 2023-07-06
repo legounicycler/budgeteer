67302394065313f231c8d9d074bc462c495822cd
Anthony Limiero <normaljoepro@gmail.com>
2023-07-06 19:12:06 -0400

    Reconcile balances display properly for pending transactions

--- static/js/init.js
+++ static/js/init.js
@@ -208,22 +208,41 @@
     //------------- FUNCTIONAL JS -------------//
 
     function refresh_reconcile() {
-      console.log("REFRESH RECONCILE!");
-      var reconcile_balance = text_to_num($("#page-total").text())
+      var page_total = text_to_num($("#page-total").text())
+      var reconcile_balance = page_total;
       var i = 0;
-      $("#transactions-bin .balance").each(function() {
-        var amt = text_to_num($(this).text());
-        var $reconcile_span = $(this).parent().parent().find(".reconcile-span");
+      var pending_transactions = [];
+      $("#transactions-bin .transaction-row").each(function(index,elem) {
         if ($(this).hasClass("pending")) {
-          console.log($(this));
-        }
-        if ($(this).hasClass("neutral")) {
-          $reconcile_span.text(balance_format(reconcile_balance))
+          //Add any pending transactions to an array to deal with after this loop
+          pending_transactions.push($(this));
         } else {
-          $reconcile_span.text(balance_format(reconcile_balance))
-          reconcile_balance = reconcile_balance - amt
+          $balance = $(this).find(".balance");
+          $reconcile_span = $(this).find(".reconcile-span");
+          var amt = text_to_num($balance.text());
+          if ($balance.hasClass("neutral")) {
+            $reconcile_span.text(balance_format(reconcile_balance))
+          } else {
+            $reconcile_span.text(balance_format(reconcile_balance))
+            reconcile_balance = reconcile_balance - amt
+          }
         }
-      })
+      });
+
+      // If there are pending transactions, update their reconcile balances in reverse order
+      var pending_reconcile_balance = page_total;
+      pending_transactions.reverse().forEach(function($transaction_row) {
+        $balance = $transaction_row.find(".balance");
+        $reconcile_span = $transaction_row.find(".reconcile-span");
+        var amt = text_to_num($balance.text());
+        if ($balance.hasClass("neutral")) {
+          $reconcile_span.text(balance_format(pending_reconcile_balance))
+        } else {
+          pending_reconcile_balance = pending_reconcile_balance + amt
+          $reconcile_span.text(balance_format(pending_reconcile_balance))
+        }
+      });
+      
     }
 
     // Formats a number into a string with a "$" and "-" if necessary
