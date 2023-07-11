5168d7dd9eec27b75daaae5824b1da4ba70381ce
Anthony Limiero <normaljoepro@gmail.com>
2023-07-11 01:17:59 -0400

    Renames old_code to temporary_code and adds add_pending.py

---
+++ add_pending.py
@@ -0,0 +1,23 @@
+from database import *
+import platform
+
+platform = platform.system()
+if platform == 'Windows':
+    database = 'C:\\Users\\norma\\Documents\\Github\\budgeteer\\database.sqlite'
+else:
+    database = '/home/alimiero/database.sqlite'
+
+conn = sqlite3.connect(database, check_same_thread=False)
+c = conn.cursor()
+
+with conn:
+    c.execute("""
+        ALTER TABLE transactions
+        ADD COLUMN pending BOOL NOT NULL DEFAULT 0
+        """)
+    
+    # c.execute("UPDATE transactions SET pending=0 WHERE date(day) <= date('now', 'localtime')")
+    c.execute("UPDATE transactions SET pending=1 WHERE date(day) > date('now', 'localtime')")
+
+
+
