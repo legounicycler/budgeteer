dde2fbb01b5e67c2ee66fa4f3ce5895502ef63c4
Anthony Limiero <normaljoepro@gmail.com>
2024-07-18 22:27:27 -0400

    Completed registration unit tests

--- test/unit/test_auth.py
+++ test/unit/test_auth.py
@@ -24,6 +24,16 @@ def get_csrf_token(client):
 def mock_verify_recaptcha(response):
     return {'score': 0.9}
 
+def remove_key(d, key):
+    r = dict(d)
+    del r[key]
+    return r
+
+def modify_dict(d, key, value):
+    r = dict(d)
+    r[key] = value
+    return r
+
 # endregion HELPERS
 
 # region FIXTURES
@@ -68,12 +78,12 @@ def mock_verify_recaptcha(mocker):
 
 def test_email_send(mail_instance):
     with mail_instance.record_messages() as outbox:
-        msg = Message(subject="Test email", recipients=['normaljoepro@gmail.com'], body="Test email body")
+        msg = Message(subject="Test email", recipients=['email@example.com'], body="Test email body")
         mail_instance.send(msg)
         assert len(outbox) == 1
         assert outbox[0].subject == 'Test email'
         assert outbox[0].body == 'Test email body'
-        assert outbox[0].recipients == ["normaljoepro@gmail.com"]
+        assert outbox[0].recipients == ["email@example.com"]
 
 def test_post_to_nonexistent_route(client):
     """
@@ -152,6 +162,8 @@ def test_login_get_authenticated_and_unconfirmed_user(logged_in_user_client):
     assert b'<form id="register-form"' in response.data
     assert b'<form id="forgot-password-form"' in response.data
 
+# --Login API tests--
+
 def test_api_login_nonexistent_user(client, mock_verify_recaptcha):
     """
     Test POSTing data to the login form for a user that doesn't exist.
@@ -168,8 +180,7 @@ def test_api_login_nonexistent_user(client, mock_verify_recaptcha):
             'password': 'password',
             'csrf_token': csrf_token,
             'g-recaptcha-response': 'dummy-response'
-            },
-        follow_redirects=True
+            }
     )
 
     # 2. Verify the response
@@ -192,8 +203,7 @@ def test_api_login_unconfirmed_user(logged_in_user_client, mock_verify_recaptcha
             'password': 'password',
             'csrf_token': csrf_token,
             'g-recaptcha-response': 'dummy-response'
-            },
-        follow_redirects=True
+            }
     )
 
     # 2. Verify the response redirects to the confirm page
@@ -220,8 +230,7 @@ def test_api_login_confirmed_user(logged_in_user_client, mock_verify_recaptcha):
             'password': 'password',
             'csrf_token': csrf_token,
             'g-recaptcha-response': 'dummy-response'
-            },
-        follow_redirects=True
+            }
     )
 
     # 3. Verify the response redirects to the home page (i.e. login_success=true)
@@ -235,7 +244,7 @@ def test_api_login_malformed_data(client, mock_verify_recaptcha):
     # 1. Post data to the login form with a malformed email address
     response = client.post('/api/login', data={'email': "email@example", 'password': 'password', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
     assert response.status_code == 200
-    assert b'{"login_success":false,"message":"No user with that email exists!"}' in response.data
+    assert b'"errors":{"email":["Invalid email address."]},"login_success":false' in response.data
 
     # 2. Post data to the login form with no email
     response = client.post('/api/login', data={'password': 'password', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
@@ -243,17 +252,17 @@ def test_api_login_malformed_data(client, mock_verify_recaptcha):
     assert b'"errors":{"email":["This field is required."]}' in response.data
 
     # 3. Post data to the login form with no password
-    response = client.post('/api/login', data={'email': 'email@example', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
+    response = client.post('/api/login', data={'email': 'email@example.com', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
     assert response.status_code == 200
     assert b'"errors":{"password":["This field is required."]}' in response.data
 
     # 4. Post data to the login form with a malformed password (too long)
-    response = client.post('/api/login', data={'email': 'email@example', 'password': 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
+    response = client.post('/api/login', data={'email': 'email@example.com', 'password': 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
     assert response.status_code == 200
     assert b'{"errors":{"password":["Password must be between 8 and 32 characters long"]}' in response.data
 
     # 5. Post data to the login form with a malformed password (too short)
-    response = client.post('/api/login', data={'email': 'email@example', 'password': 'abc', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
+    response = client.post('/api/login', data={'email': 'email@example.com', 'password': 'abc', 'csrf_token': csrf_token,'g-recaptcha-response': 'dummy-response'})
     assert response.status_code == 200
     assert b'{"errors":{"password":["Password must be between 8 and 32 characters long"]}' in response.data
 
@@ -275,8 +284,7 @@ def test_api_login_incorrect_password(client, mock_verify_recaptcha):
             'password': 'incorrectpassword',
             'csrf_token': csrf_token,
             'g-recaptcha-response': 'dummy-response'
-            },
-        follow_redirects=True
+            }
     )
 
     # 2. Verify the response redirects to the home page
@@ -291,22 +299,18 @@ def test_api_login_bad_recaptcha(client):
     response = client.post(
         '/api/login',
         data={
-            'email': 'email@example',
+            'email': 'email@example.com',
             'password': 'password',
             'csrf_token': csrf_token,
             'g-recaptcha-response': 'bad-response'
-            },
-        follow_redirects=True
+            }
     )
 
     # 2. Verify the response redirects to the home page
     assert response.status_code == 200
     assert b'{"login_success":false,"message":"Error: An unknown error occurred!"}' in response.data
 
-# --Create account form--
-# Malformed data: Missing firstname, missing lastname, missing email, missing password1, missing password2
-# passwords don't match, too short firstname/lastname, malformed email, too short/long password1/password2
-# Email already exists
+# --Create account form tests--
 
 def test_create_account_form_success(client, mail_instance, mock_verify_recaptcha):
     with mail_instance.record_messages() as outbox:
@@ -325,8 +329,7 @@ def test_create_account_form_success(client, mail_instance, mock_verify_recaptch
                 'confirm_password': 'password',
                 'csrf_token': csrf_token,
                 'g-recaptcha-response': 'dummy-response'
-                },
-            follow_redirects=True
+                }
         )
 
         # 2. Verify the response
@@ -348,13 +351,12 @@ def test_create_account_email_already_exists(client, mock_verify_recaptcha):
         data={
             'new_first_name': 'firstname',
             'new_last_name': 'lastname',
-            'new_email': 'email@example',
+            'new_email': 'email@example.com',
             'new_password': 'password',
             'confirm_password': 'password',
             'csrf_token': csrf_token,
             'g-recaptcha-response': 'dummy-response'
-            },
-        follow_redirects=True
+            }
     )
 
     # 2. Post data to the login form with an email for a user that already is registered with the same email
@@ -363,18 +365,106 @@ def test_create_account_email_already_exists(client, mock_verify_recaptcha):
         data={
             'new_first_name': 'firstname',
             'new_last_name': 'lastname',
-            'new_email': 'email@example',
+            'new_email': 'email@example.com',
             'new_password': 'password',
             'confirm_password': 'password',
             'csrf_token': csrf_token,
             'g-recaptcha-response': 'dummy-response'
-            },
-        follow_redirects=True
+            }
     )
 
     # 2. Verify the response redirects to the home page
     assert response.status_code == 200
     assert b'{"message":"A user with that email already exists!","register_success":false}' in response.data
+
+def test_create_account_malformed_data(client, mock_verify_recaptcha):
+    # 0. Get the CSRF token from the hidden input on the login page
+    csrf_token = get_csrf_token(client)
+
+    post_data = {
+        'new_first_name': 'firstname',
+        'new_last_name': 'lastname',
+        'new_email': 'email@example.com',
+        'new_password': 'password',
+        'confirm_password': 'password',
+        'csrf_token': csrf_token,
+        'g-recaptcha-response': 'dummy-response'
+    }
+
+    # 1. Post data with missing firstname
+    response = client.post('/register', data = remove_key(post_data, 'new_first_name'))
+    assert response.status_code == 200
+    assert b'"errors":{"new_first_name":["This field is required."]},"login_success":false' in response.data
+
+    #2. Post data with missing lastname
+    response = client.post('/register', data = remove_key(post_data, 'new_last_name'))
+    assert response.status_code == 200
+    assert b'"errors":{"new_last_name":["This field is required."]},"login_success":false' in response.data
+
+    #3. Post data with missing email
+    response = client.post('/register', data = remove_key(post_data, 'new_email'))
+    assert response.status_code == 200
+    assert b'"errors":{"new_email":["This field is required."]},"login_success":false' in response.data
+
+    #4. Post data with missing new_password
+    response = client.post('/register', data = remove_key(post_data, 'new_password'))
+    assert response.status_code == 200
+    assert b'"errors":{"confirm_password":["Passwords must match"],"new_password":["This field is required."]},"login_success":false' in response.data
+
+    #5. Post data with missing confirm_password
+    response = client.post('/register', data = remove_key(post_data, 'confirm_password'))
+    assert response.status_code == 200
+    assert b'"errors":{"confirm_password":["This field is required."],"new_password":["Passwords must match"]},"login_success":false' in response.data
+
+    #6. Post data with too long firstname
+    response = client.post('/register', data = modify_dict(post_data, "new_first_name", "a" * 51))
+    assert response.status_code == 200
+    assert b'"errors":{"new_first_name":["Field must be between 2 and 50 characters long."]},"login_success":false' in response.data
+
+    #7. Post data with too short firstname
+    response = client.post('/register', data = modify_dict(post_data, "new_first_name", "a"))
+    assert response.status_code == 200
+    assert b'"errors":{"new_first_name":["Field must be between 2 and 50 characters long."]},"login_success":false' in response.data
+
+    #8. Post data with too long lastname
+    response = client.post('/register', data = modify_dict(post_data, "new_last_name", "a" * 51))
+    assert response.status_code == 200
+    assert b'"errors":{"new_last_name":["Field must be between 2 and 50 characters long."]},"login_success":false' in response.data
+
+    #9. Post data with too short lastname
+    response = client.post('/register', data = modify_dict(post_data, "new_last_name", "a"))
+    assert response.status_code == 200
+    assert b'"errors":{"new_last_name":["Field must be between 2 and 50 characters long."]},"login_success":false' in response.data
+
+    #10. Post data with malformed email
+    response = client.post('/register', data = modify_dict(post_data, "new_email", "email@example"))
+    assert response.status_code == 200
+    assert b'"errors":{"new_email":["Invalid email address."]},"login_success":false' in response.data
+
+    #11. Post data with too short new_password
+    response = client.post('/register', data = modify_dict(post_data, "new_password", "a"))
+    assert response.status_code == 200
+    assert b'{"errors":{"confirm_password":["Passwords must match"],"new_password":["Passwords must match","Password must be between 8 and 32 characters long"]},"login_success":false' in response.data
+
+    #12. Post data with too long new_password
+    response = client.post('/register', data = modify_dict(post_data, "new_password", "a" * 33))
+    assert response.status_code == 200
+    assert b'{"errors":{"confirm_password":["Passwords must match"],"new_password":["Passwords must match","Password must be between 8 and 32 characters long"]},"login_success":false' in response.data
+
+    #13. Post data with too short confirm_password
+    response = client.post('/register', data = modify_dict(post_data, "confirm_password", "a"))
+    assert response.status_code == 200
+    assert b'{"errors":{"confirm_password":["Passwords must match","Password must be between 8 and 32 characters long"],"new_password":["Passwords must match"]},"login_success":false' in response.data
+
+    #14. Post data with too long confirm_password
+    response = client.post('/register', data = modify_dict(post_data, "confirm_password", "a" * 33))
+    assert response.status_code == 200
+    assert b'{"errors":{"confirm_password":["Passwords must match","Password must be between 8 and 32 characters long"],"new_password":["Passwords must match"]},"login_success":false' in response.data
+
+    #15. Post data with mismatched passwords
+    response = client.post('/register', data = modify_dict(post_data, "confirm_password", "mismatched_password"))
+    assert response.status_code == 200
+    assert b'{"errors":{"confirm_password":["Passwords must match"],"new_password":["Passwords must match"]},"login_success":false,"message":"Register form validation failed!"}' in response.data
 
 # --Confirm email route--
 # Something
