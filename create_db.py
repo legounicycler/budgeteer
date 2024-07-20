6f386aad81996e9c63ec502810690a7580a8ff3e
Anthony Limiero <normaljoepro@gmail.com>
2024-07-20 01:39:21 -0400

    Updates requirements.txt and create_db.py

--- create_db.py
+++ create_db.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         database = 'C:\\Users\\norma\\Documents\\Github\\budgeteer\\database.sqlite'
     else:
         database = '/home/opc/database.sqlite'
-    conn = sqlite3.connect(database, check_same_thread=False)
-    c = conn.cursor()
-    create_tables(conn, c)
-    conn.close()
+    db = Database(database)
+    db.get_conn()
+    db.create_tables()
+    db.close_conn()
