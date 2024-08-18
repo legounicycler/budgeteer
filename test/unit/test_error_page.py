722eb5d3f3d0e45c7c2d5430da249cd1e44e2f39
Anthony Limiero <normaljoepro@gmail.com>
2024-08-18 17:24:38 -0400

    Some more comments for test_error_page

--- test/unit/test_error_page.py
+++ test/unit/test_error_page.py
@@ -3,11 +3,12 @@ from test.unit.conftest import get_csrf_token
 from flask_login import current_user
 from blueprints.auth import confirm_user
 
+# TODO: Move this to conftest since it will also used in test_main.py (And move the one in test_main.py)
 @pytest.fixture
 def mock_get_uuid_from_cookie(mocker):
     return mocker.patch('blueprints.main.get_uuid_from_cookie', return_value=current_user.id)
 
-# TODO: Move this to conftest since it's also used in auth.py
+# TODO: Move this to conftest since it's also used in test_auth.py (And move the one in test_auth.py )
 @pytest.fixture
 def mail_instance(app):
     return app.mail
@@ -37,3 +38,27 @@ def test_bug_report_send_success(logged_in_user_client, mock_get_uuid_from_cooki
         assert len(outbox) == 2
         assert outbox[0].subject == 'Budgeteer: Bug Report from email@example.com'
         assert outbox[1].subject == 'Budgeteer: Your Bug Report Has Been Received'
+
+def test_bug_report_success_no_screenshot(logged_in_user_client, mock_get_uuid_from_cookie, mail_instance):
+    """
+    Test POSTing data to the bug report form with a valid name, email address, description, and no screenshot
+    """
+    pass
+
+def test_bug_report_send_failure_malformed_textual_data(logged_in_user_client, mock_get_uuid_from_cookie):
+    """
+    Test cases for bad/empty name, bad/empty email, bad/empty description
+    """
+    pass
+
+def test_bug_report_send_failure_invalid_filetype(logged_in_user_client, mock_get_uuid_from_cookie):
+    """
+    Test case for when a user submits non-image filetypes
+    """
+    pass
+
+def test_bug_report_send_failure_invalid_filesize(logged_in_user_client, mock_get_uuid_from_cookie):
+    """
+    Test case for when a user submits files that are too large
+    """
+    pass
