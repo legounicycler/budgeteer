92cc8cfeb58653dc53f81518ec9c79d325d3367c
Anthony Limiero <normaljoepro@gmail.com>
2026-02-11 16:29:06 -0500

    Improvements to docs when cloning budgeteer from fresh machine

--- create_db.py
+++ create_db.py
@@ -5,6 +5,18 @@ from blueprints.auth import generate_uuid
 from database import User, insert_user, confirm_user
 
 create_default_user = False
+
+def create_database():
+    db = Database(database)
+    db.get_conn()
+    db.create_tables()
+    if create_default_user:
+        (new_password_hash, new_password_salt) = User.hash_password("password")
+        new_user = User(generate_uuid(), "email@example.com", new_password_hash, new_password_salt, "Firstname", "Lastname", datetime.now())
+        insert_user(new_user)
+        confirm_user(new_user)
+        print(f"Default user created with email: '{new_user.email}' and password: 'password'")
+    db.close_conn()
     
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -18,15 +30,14 @@ if __name__ == "__main__":
 
     try:
         with open(database, "x") as f:
-            db = Database(database)
-            db.get_conn()
-            db.create_tables()
-            if create_default_user:
-                (new_password_hash, new_password_salt) = User.hash_password("password")
-                new_user = User(generate_uuid(), "email@example.com", new_password_hash, new_password_salt, "Firstname", "Lastname", datetime.now())
-                insert_user(new_user)
-                confirm_user(new_user)
-            db.close_conn()
+            create_database()
     except FileExistsError:
         print(f"Database file already exists at {database}")
-        print("If you want to recreate the database, please delete the existing file and run this script again.")
+        print("The file database.sqlite already exists. Would you like to overwrite it? (y/n)")
+        choice = input().lower()
+        if choice == "y":
+            with open(database, "w") as f:
+                create_database()
+        else:
+            print("Exiting without overwriting the database.")
+            sys.exit(0)
